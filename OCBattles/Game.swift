69acae57fa9278c62f361530aa1789e27810be74
willf80 <willyfkouadio@gmail.com>
2018-04-03 21:07:25 +0000

    Add class Game. Setting up the actions of the game. Possibility to start or restart the game

---
+++ OCBattles/Game.swift
@@ -0,0 +1,20 @@
+//
+//  Menu.swift
+//  OCBattles
+//
+//  Created by WILLY Falone Kouadio on 03/04/2018.
+//  Copyright © 2018 WILLY Falone Kouadio. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    func startNewGame() -> Void {
+        
+    }
+    
+    func restartGame() -> Void {
+        
+    }
+}
