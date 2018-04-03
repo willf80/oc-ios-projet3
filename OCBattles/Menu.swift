49ba663712f5442a26a1a8988c9be3442be31640
willf80 <willyfkouadio@gmail.com>
2018-04-03 22:42:13 +0000

    add class menu with func showHomeMenu

---
+++ OCBattles/Menu.swift
@@ -0,0 +1,19 @@
+//
+//  Menu.swift
+//  OCBattles
+//
+//  Created by WILLY Falone Kouadio on 03/04/2018.
+//  Copyright © 2018 WILLY Falone Kouadio. All rights reserved.
+//
+
+import Foundation
+
+class Menu {
+    //  Create game menu
+    //  Show player status
+    
+    func showHomeMenu() -> Void {
+        //  1. New Game
+        //  2. Stop current game
+    }
+}
