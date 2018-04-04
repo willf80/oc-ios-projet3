cd303b29f635c9aa114bb58966823eeaa082d343
willf80 <willyfkouadio@gmail.com>
2018-04-04 16:15:00 +0000

    Loop of game added

--- OCBattles/Game.swift
+++ OCBattles/Game.swift
@@ -10,8 +10,19 @@ import Foundation
 
 class Game {
     
+    //  Indicates whether the game is over or not
+    var quit = false
+    
     func startNewGame() -> Void {
         
+        //  Loop of game
+        //  Do not quit while new part is not ended
+        //  The end of the game may be due to the fact that:
+        //  - One of the two players wins the game
+        //  - One of the players decides to stop the game
+        repeat {
+            
+        }while !quit
     }
     
     func restartGame() -> Void {
