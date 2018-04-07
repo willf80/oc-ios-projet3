792d74b4eeca851a713d248410e373fe241673f8
willf80 <willyfkouadio@gmail.com>
2018-04-07 03:00:17 +0000

    Weapon element enumerated and definition of WeaponStore

--- OCBattles/Constants.swift
+++ OCBattles/Constants.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 
-//
+//All IDs of the weapons to make the source code easily readable
+enum WeaponNameID: String{
+    case EPEE = "EPEE"
+    case CARE_SHEET = "CARE_SHEET"
+    case CUDGEL = "CUDGEL"
+    case AXE = "AXE"
+}
+
 class Constants {
+    //  Define all available weapons
+    static let WeaponStore = [WeaponNameID.EPEE.rawValue : WeaponAttack(name: "Epée", damage: 10),
+                              WeaponNameID.CARE_SHEET.rawValue : WeaponCare(name: "Feuille de soin", care: 6),
+                              WeaponNameID.CUDGEL.rawValue : WeaponAttack(name: "Gourdin", damage: 3),
+                              WeaponNameID.AXE.rawValue : WeaponAttack(name: "Hache", damage: 7)]
     
     //  Read integer line
     static func readInteger() -> Int? {
@@ -21,4 +33,5 @@ class Constants {
         
         return input
     }
+    
 }
